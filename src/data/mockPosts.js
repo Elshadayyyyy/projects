845e10d76fc7767298bf36da3231c9c81ee62bbf
Elshadayyyyy <alemelshaday036@gmail.com>
2025-10-25 17:40:59 +0300

    updated the image issue

--- src/data/mockPosts.js
+++ src/data/mockPosts.js
@@ -2,14 +2,13 @@ import sampleMakeup from "../assets/sampleMakeup.jpg";
 import sampleCrochet from "../assets/samplecrochet.jpg";
 import sampleClothes from "../assets/shirt.jpg";
 import sampleShoes from "../assets/shoes.jpg";
-
 export const mockPosts = [
   {
     id: 1,
     authorUsername: "sample Makeup Store",
     timestamp: new Date().toISOString(),
     caption: "Check out this awesome makeup kit!",
-    image: sampleMakeup,
+    image: "/assets/sampleMakeup.jpg",
     likes: [],
     comments: [],
     price: 100,
@@ -19,27 +18,27 @@ export const mockPosts = [
     authorUsername: "hana crochet",
     timestamp: new Date().toISOString(),
     caption: "I do crochet and have it in hand. Order via cart!",
-    image: sampleCrochet,
+    image: "/assets/samplecrochet.jpg",
     likes: [],
     comments: [],
     price: 200,
   },
-    {
+  {
     id: 3,
     authorUsername: "lemlem clothes",
     timestamp: new Date().toISOString(),
     caption: "instock!",
-    image: sampleClothes,
+    image: "/assets/shirt.jpg",
     likes: [],
     comments: [],
     price: 600,
   },
-    {
+  {
     id: 4,
     authorUsername: "lemlem clothes",
     timestamp: new Date().toISOString(),
     caption: "we are having a hugeeeee sale buy now!",
-    image: sampleShoes,
+    image: "/assets/shoes.jpg",
     likes: [],
     comments: [],
     price: 2500,
